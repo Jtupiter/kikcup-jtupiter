d987352bacf4bb5b61ae03a40b55373d56044916
xycharleslin <xycharleslin@gmail.com>
2013-09-17 20:36:08 -0400

    testing homepage

--- web/js/main.js
+++ web/js/main.js
@@ -1,17 +1,32 @@
 (function (App) {
+
+    var photoUrl;
+    var message;
+    var messagePlaceholder = "(optional: enter a message)"
+
     App.populator('home', function (page) {
         $(page)
-            .on('click', "#camBtn",function(){
-                cards.photo.get(function (photos)  {
-                        if (!photos) {
+            .on('click', "#camBtn", function(){
+                cards.photo.get(function (photos) {
+                    if (!photos) {
 
-                        } else {
-                            $('#photo').attr('src', photos[0]);
-                        }
+                    } else {
+                        $('#photo').attr('src', photos[0]);
+                        photoUrl = photos[0];
+                    }
                 });
-
         });
+        $(page)
+            .on('click', "#sendBtn", function() {
+                if ($('#message').val() == messagePlaceholder) {
+                    message = '';
+                } else {
+                    message = $('#message').val();
+                }
+                App.load('contacts');
+            });
     });
+
 
     try {
         App.restore();
