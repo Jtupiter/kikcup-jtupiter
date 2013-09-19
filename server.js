17eeef1a92704d776818298f989a173b5a4a25ec
Jordan Upiter <jordan.upiter@gmail.com>
2013-09-19 01:49:47 -0400

    Added getUser back in

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ server.listen(process.env.PORT || 5000);
 
 app.use(express.static(__dirname + '/web'));
 
-var mongoUrl = process.env.MONGOHQ_URL || "mongodb://jtupiter:jtupiter@paulo.mongohq.com:10072/app18070711";
+var mongoUrl = process.env.MONGOHQ_URL || "mongodb://localhost";
 var db = mongojs.connect(mongoUrl);
 
 app.get('/', function(req, res){
