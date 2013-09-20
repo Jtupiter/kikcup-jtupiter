17a0575a7d4253daabbe7f25600c935670d1a526
Jordan Upiter <jordan.upiter@gmail.com>
2013-09-20 01:34:02 -0400

    Got rid of Angular now, will use ajax requests

--- web/js/main.js
+++ web/js/main.js
@@ -1,60 +1,60 @@
-function PhotoBookCtrl($scope, $http) {
-     /*cards.kik.getUser(function (user) {
-        if ( !user ) {
-            // user denied access to their information
-            return;
-        }
-        else {
-            $scope.user = $http.get('/user/' + user.username);
-        }
-    });*/
+(function (App) {
     mock_data = {
         id: 1, 
         name: "jtupiter", 
         groups:[{name: "The Boys", photos:["url_boy1", "url_boy2"]}, {name: "The Girls", photos:["url_girl1", "url_girl2"]}]}
-    $scope.user = mock_data;
-    for (var i = 0; i < $scope.user.groups.length;i++){
-        $('#group-edit-list').append('<li class="group">'+ $scope.user.groups[i].name +'</li>');
-        $('#group-list').append('<li class="receiver">'+ $scope.user.groups[i].name +'</li>');
+    user = mock_data;
+    for (var i = 0; i < user.groups.length;i++){
+        $('#group-edit-list').append('<li class="group">'+ user.groups[i].name +'</li>');
+        $('#group-list').append('<li class="receiver">'+ user.groups[i].name +'</li>');
     }
-    (function (App) {
-        
-        App.populator('home', function (page) {
-            $(page)
-                .on('click', "#btn-cam", function(){
-                    cards.photo.get(function (photos) {
-                        if (!photos) {
+    
+    App.populator('home', function (page) {
+        $(page)
+            .on('click', "#btn-cam", function(){
+                cards.photo.get(function (photos) {
+                    if (!photos) {
 
-                        } else {
-                            $('#photo').attr('src', photos[0]);
-                        }
-                    })
-                });
-            $(page)
-                .on('click', "#btn-post", function() {
-                    var photoUrl = $('#photo').attr('src');
-                    App.load('test', { photo: photoUrl });
-                });
-        });
-
-        App.populator('post-to-group', function (page, json) {
-            $(page)
-                .on('click', ".receiver", function(){
-                    if ($(this).hasClass("selected")) {
-                        $(this).removeClass("selected");
                     } else {
-                        $(this).addClass("selected");
+                        $('#photo').attr('src', photos[0]);
                     }
-                });
-        });
+                })
+            });
+        $(page)
+            .on('click', "#btn-post", function() {
+                var photoUrl = $('#photo').attr('src');
+                App.load('test', { photo: photoUrl });
+            });
+    });
 
-        try {
-            App.restore();
-        } catch (err) {
-            App.load('home');
-        }  
+    App.populator('post-to-group', function (page, json) {
+        $(page)
+            .on('click', ".receiver", function(){
+                if ($(this).hasClass("selected")) {
+                    $(this).removeClass("selected");
+                } else {
+                    $(this).addClass("selected");
+                }
+            });
+    });
 
-        
-    })(App);
+    App.populator('view-groups', function (page, json) {
+        $(page)
+            .on('click', ".edit-groups", function(){
+                if ($(this).hasClass("selected")) {
+                    $(this).removeClass("selected");
+                } else {
+                    $(this).addClass("selected");
+                }
+            });
 
-}
+    });
+
+    try {
+        App.restore();
+    } catch (err) {
+        App.load('home');
+    }  
+
+    
+})(App);
