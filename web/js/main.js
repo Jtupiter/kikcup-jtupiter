14f8d1378860d92373fef098b1cf6894e26359da
xycharleslin <xycharleslin@gmail.com>
2013-09-20 04:23:57 -0400

    merged commits; redid homepage

--- web/js/main.js
+++ web/js/main.js
@@ -40,27 +40,41 @@
     });
 
     App.populator('post-to-group', function (page, json) {
-        $(page)
-            .on('click', ".receiver", function(){
-                var group_name = $(this).text();
-                var group_id = $(this).data('group');
-                App.load('groupphotos', { id: group_id, group_name : group_name });
-            });
-    });
-
-    App.populator('groupphotos', function (page, json) {
-        $(page).find('.app-title').text(json.group_name);
-        $.post( "/group/" + json.id, function(data) {
-            user_data = data;
+        $(page).on('click', ".receiver", function(){
+            // PUSH PHOTOURL TO GROUP DATABASE
+            var group_name = $(this).text();
+            var group_id = $(this).data('group');
+            App.load('groupphotos', { id : group_id, group_name : group_name });
         });
     });
 
     App.populator('view-groups', function (page, json) {
-        $(page)
-            .on('click', ".receiver", function(){
-                var groupname = $(this).text();
-                App.load('groupphotos', { group : groupname });
-            });
+        $(page).on('click', ".group", function(){
+            var group_name = $(this).text();
+            App.load('groupphotos', { group_name : group_name });
+        });
+    });
+
+    App.populator('groupphotos', function (page, json) {
+        $(page).find('.app-title').text(json.group_name);
+        /*
+        $.post( "/group/" + json.id, function(data) {
+            user_data = data;
+        });
+        */
+        // ASSUME I HAVE IMAGE ARRAY
+        var imagearray = ['img/iphoto.jpg', 'img/iphoto.png', 'img/test1.jpg', 'img/test2.png'];
+        for (var i = 0; i < imagearray.length; i++) {
+            $(page).find('.app-content').append('<div class="groupphoto" image="' + imagearray[i] + '" style="background-image: url(\'' + imagearray[i] + '\'); background-size: 100%;"></div>');
+        }
+        $(page).on('click', ".groupphoto", function() {
+            var photoUrl = $(this).attr('image');
+            App.load('photopage', { photo : photoUrl });
+        });
+    });
+
+    App.populator('photopage', function (page, json) {
+        $(page).find('#pagephoto').attr('src', json.photo);
     });
 
     try {
