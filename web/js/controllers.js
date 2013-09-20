639cef4674d45866c86489f46c4ecf53c28fdf9d
Jordan Upiter <jordan.upiter@gmail.com>
2013-09-20 00:48:02 -0400

    Moved angular implementation cause Kik doesn't like it

--- web/js/controllers.js
+++ web/js/controllers.js
@@ -1,17 +1,21 @@
-function PingCtrl($scope, $http) {
-	/*cards.kik.getUser(function (user) {
+/*function PhotoBookCtrl($scope, $http) {
+    /*cards.kik.getUser(function (user) {
         if ( !user ) {
             // user denied access to their information
             return;
         }
         else {
-    		$scope.user = $http.get('/user/' + user.username);
+            $scope.user = $http.get('/user/' + user.username);
         }
-    });*/
-	mock_data = {
-		id: 1, 
-		name: "jtupiter", 
-		groups:[{name: "The Boys", photos:["url_boy1", "url_boy2"]}, {name: "The Girls", photos:["url_girl1", "url_girl2"]}]}
-	$scope.user = mock_data
+    });
+    mock_data = {
+        id: 1, 
+        name: "jtupiter", 
+        groups:[{name: "The Boys", photos:["url_boy1", "url_boy2"]}, {name: "The Girls", photos:["url_girl1", "url_girl2"]}]}
+    $scope.user = mock_data
+    for (var i = 0; i < $scope.user.groups.length;i++){
+        $('#group-edit-list').append('<li class="group">'+ $scope.user.groups[i].name +'</li>');
+        $('#group-list').append('<li class="receiver">'+ $scope.user.groups[i].name +'</li>');
+    }
 
-};
+}*/
