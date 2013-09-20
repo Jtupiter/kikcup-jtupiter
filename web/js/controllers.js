eba9813893c969fceaf957803c8671edf7a52797
Jordan Upiter <jordan.upiter@gmail.com>
2013-09-20 00:14:35 -0400

    Added groups page

--- web/js/controllers.js
+++ web/js/controllers.js
@@ -1,5 +1,5 @@
 function PingCtrl($scope, $http) {
-	cards.kik.getUser(function (user) {
+	/*cards.kik.getUser(function (user) {
         if ( !user ) {
             // user denied access to their information
             return;
@@ -7,6 +7,11 @@ function PingCtrl($scope, $http) {
         else {
     		$scope.user = $http.get('/user/' + user.username);
         }
-    });
+    });*/
+	mock_data = {
+		id: 1, 
+		name: "jtupiter", 
+		groups:[{name: "The Boys", photos:["url_boy1", "url_boy2"]}, {name: "The Girls", photos:["url_girl1", "url_girl2"]}]}
+	$scope.user = mock_data
 
 };
